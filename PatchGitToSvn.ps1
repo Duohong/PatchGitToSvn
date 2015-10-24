5d493ef023dd04d8978c1ef15ec868402340f2dc
Duohong <duohongopen@gmail.com>
2015-10-23 20:55:21 -0400

    PatchGitToSvn added

---
+++ PatchGitToSvn.ps1
@@ -0,0 +1,4 @@
+$SvnRevisionOneLine = (git svn log --oneline -1) | Out-String
+$pos = $SvnRevisionOneLine.IndexOf(" | ")
+$SvnRevision = $SvnRevisionOneLine.Substring(1, $pos-1)
+(Get-Content ("patch.diff")) -notmatch "diff[ ]--git[ ]a/" | Where { $_ -notmatch "^index[ ]\w{7}[.]{2}\w{7}[ ]\d{5}" } | %{if($_ -match [regex]::escape("--- a/")){$_ += "	(revision $SvnRevision)"}; $_} | ForEach-Object { $_ -replace [regex]::escape("--- a/"), "--- " } | %{if($_ -match [regex]::escape("+++ b/")){$_ += "	(working copy)"}; $_} | ForEach-Object { $_ -replace [regex]::escape("+++ b/"), "+++ " } | Set-Content ("patchSvn.diff")
