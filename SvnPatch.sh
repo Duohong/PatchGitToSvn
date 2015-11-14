f4aa5f7f6ebb1647e46cf5996e64339ad133b0f2
Duohong <duohongopen@gmail.com>
2015-11-14 18:00:51 -0500

    SvnRevision updated to head

--- SvnPatch.sh
+++ SvnPatch.sh
@@ -1,7 +1,6 @@
 # get SvnRevision first
-SvnRevisionOneLine=$(git svn log --oneline -1)
-SvnRevision=${SvnRevisionOneLine%% *}
-#echo "${SvnRevision#r}"
+SvnRevision=${git svn find-rev $(git rev-parse head))}
+#echo "${SvnRevision}"
 # --cached for staged files
 # with prefix to sure
-git diff --cached | sed -e "/^diff --git a/d" -e "/^index[ ]\w\{7\}[.][.]\w\{7\}/d" -e  "/^--- a/ s|$|	(revision ${SvnRevision#r})|" -e 's|^--- a/|--- |' -e '/^+++ b/ s/$/	(working copy)/' -e 's|^+++ b/|+++ |' > patch.diff.svn
+git diff --cached | sed -e "/^diff --git a/d" -e "/^index[ ]\w\{7\}[.][.]\w\{7\}/d" -e  "/^--- a/ s|$|	(revision ${SvnRevision})|" -e 's|^--- a/|--- |' -e '/^+++ b/ s/$/	(working copy)/' -e 's|^+++ b/|+++ |' > patch.diff.svn
